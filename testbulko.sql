ff74c204b963385b64fe0366a11d4fb3f48dcb45
kvinav <kavignonpro@gmail.com>
2017-11-29 13:39:30 +0100

    add testbulko.sql

--- testbulko.sql
+++ testbulko.sql
@@ -1,25 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  jeu. 09 nov. 2017 à 17:11
--- Version du serveur :  10.1.25-MariaDB
--- Version de PHP :  7.1.7
+-- Hôte : localhost:8889
+-- Généré le :  mer. 29 nov. 2017 à 13:37
+-- Version du serveur :  5.6.35
+-- Version de PHP :  7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Base de données :  `test_bko`
+-- Base de données :  `testbulko`
 --
 
 -- --------------------------------------------------------
@@ -63,8 +53,4 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
